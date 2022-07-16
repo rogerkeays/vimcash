0794a5b60aa0a14f9436528bfd51ad5d3a8f6b67
Roger Keays <github@rogerkeays.com>
2022-07-16 23:59:32 +0700

    implement a function to calculate an account balance in all currencies

--- vimcash.kt
+++ vimcash.kt
@@ -2,14 +2,30 @@
 
 import java.io.File
 
+fun String.getAmount(account: String): Double {
+    return substring(21, 33).toDouble() * if (indexOf(" $account ") > 33) 1 else -1
+}
+
 fun File.calculateBalance(account: String, currency: String): Double {
     val filterRegex = Regex(".{14}\\|. $currency [0-9. ]+[^ ]* $account .*")
     return readLines()
         .filter { it.matches(filterRegex) }
-        .map { it.substring(21, 33).toDouble() * if (it.indexOf(" $account ") > 33) 1 else -1 }
+        .map { it.getAmount(account) }
         .sum()
 }
  
+fun File.calculateBalances(account: String): Map<String, Double> {
+    val filterRegex = Regex(".{14}\\|. ... [0-9. ]+[^ ]* $account .*")
+    val results = mutableMapOf<String, Double>()
+    readLines()
+        .filter { it.matches(filterRegex) }
+        .forEach { 
+            val currency = it.slice(17..19)
+            results.put(currency, results.get(currency)?.plus(it.getAmount(account)) ?: it.getAmount(account)) 
+        }
+    return results
+}
+
 fun File.collectAccounts(): Set<String> {
     return readLines()
         .drop(1)
