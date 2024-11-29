893be339b4f497ee06651f9541ce1e20a1fe46ee
Roger Keays <guybrush@mail8.simplelogin.com>
2024-11-29 13:41:37 +0700

    rewrite vimcash to parse new record format: description first

--- vimcash.kt
+++ vimcash.kt
@@ -1,36 +1,61 @@
 //usr/bin/env [ $0 -nt $0.jar ] && kotlinc -d $0.jar $0; [ $0.jar -nt $0 ] && kotlin -cp $0.jar VimcashKt $@; exit 0
 
 import java.io.File
+import java.util.TreeSet
 
-fun String.parseAmount(account: String): Double {
-    return substring(22, 34).toDouble() * if (indexOf(" $account ") > 34) -1 else 1
+data class Split(
+  val seq: String,
+  val description: String,
+  val currency: String,
+  val amount: Double,
+  val debitAccount: String,
+  val creditAccount: String
+)
+
+fun String.toSplit(): Split {
+    var (seq, description, currency, amount, debitAccount, creditAccount) = toSplitRegex.find(this)!!.destructured
+    if (amount.equals("?")) amount = "0"
+    return Split(seq, description, currency, amount.toDouble(), debitAccount, creditAccount)
+}
+val toSplitRegex = Regex("^([0-9A-Z_]{13}) .\\|  ([^:]+): +([A-Za-z]+) +([-0-9.?]+) +([^ ]+) +([^ ]+)")
+
+fun Split.hasAccount(account: String): Boolean {
+    return debitAccount.equals(account) || creditAccount.equals(account)
+}
+
+fun Split.amountFor(account: String): Double {
+    if (debitAccount.equals(account)) {
+        return amount
+    } else if (creditAccount.equals(account)) {
+        return amount * -1
+    } else {
+        return 0.0
+    }
 }
 
 fun File.calculateBalance(account: String, currency: String): Double {
-    val filterRegex = Regex(".{15}\\|  $currency [0-9. ]+[^ ]* $account .*")
     return readLines()
-        .filter { it.matches(filterRegex) }
-        .map { it.parseAmount(account) }
-        .sum()
+        .map { it.toSplit() }
+        .filter { it.hasAccount(account) && it.currency.equals(currency) }
+        .sumOf { it.amountFor(account) }
 }
  
 fun File.calculateBalances(account: String): Map<String, Double> {
-    val filterRegex = Regex(".{15}\\|  ... [0-9. ]+[^ ]* $account .*")
     val results = mutableMapOf<String, Double>()
     readLines()
-        .filter { it.matches(filterRegex) }
-        .forEach { 
-            val currency = it.slice(18..20)
-            val amount = it.parseAmount(account)
-            results.put(currency, results.get(currency)?.plus(amount) ?: amount) 
-        }
+        .map { it.toSplit() }
+        .filter { it.hasAccount(account) }
+        .forEach { results.put(it.currency,
+                   results.get(it.currency)?.plus(it.amountFor(account)) ?: it.amountFor(account)) }
     return results
 }
 
 fun File.collectAccounts(): Set<String> {
-    return readLines()
-        .flatMap { it.substring(34, 52).split(' ') }
-        .toSortedSet()
+    val results = TreeSet<String>()
+    readLines()
+        .map { it.toSplit() }
+        .forEach { results.add(it.creditAccount); results.add(it.debitAccount) }
+    return results
 }
 
 fun File.reportBalances() {
